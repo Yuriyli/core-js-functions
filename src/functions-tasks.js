fea78f701c867d081388484a6aa5ff20a4de3b92
Yuriy Li <0yuriyli@gmail.com>
2026-01-14 17:44:03 +0300

    feat: add solution for getIdGeneratorFunction()

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -251,8 +251,13 @@ function partialUsingArguments(fn, ...args) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let counter = startFrom;
+
+  return () => {
+    counter += 1;
+    return counter - 1;
+  };
 }
 
 module.exports = {
